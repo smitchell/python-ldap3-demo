0bc6b308ec637e214656d7e9d9a5a1ff57bcd585
smitchell1 <steve@platformconsulting.io>
2020-05-09 14:47:04 -0500

    Added Swagger Blueprints

--- main/app.py
+++ main/app.py
@@ -1,7 +1,6 @@
-
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from flask_swagger_ui import get_swaggerui_blueprint
-# from routes import ldap_api
+from .routes import ldap_api
 
 APP = Flask(__name__)
 
@@ -18,8 +17,30 @@ SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
 APP.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
 ### end swagger specific ###
 
-# APP.register_blueprint(ldap_api.get_blueprint())
+APP.register_blueprint(ldap_api.get_blueprint())
 
+@APP.errorhandler(400)
+def handle_400_error(_error):
+    """Return a http 400 error to client"""
+    return make_response(jsonify({'error': 'Misunderstood'}), 400)
+
+
+@APP.errorhandler(401)
+def handle_401_error(_error):
+    """Return a http 401 error to client"""
+    return make_response(jsonify({'error': 'Unauthorised'}), 401)
+
+
+@APP.errorhandler(404)
+def handle_404_error(_error):
+    """Return a http 404 error to client"""
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+@APP.errorhandler(500)
+def handle_500_error(_error):
+    """Return a http 500 error to client"""
+    return make_response(jsonify({'error': 'Server error'}), 500)
 
 class Microservice:
 
