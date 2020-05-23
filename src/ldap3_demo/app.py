b4b2595d29b1a16c69857d1ffc2475dfab6eb694
smitchell1 <steve@platformconsulting.io>
2020-05-23 12:06:50 -0500

    Moved .routes import

--- src/ldap3_demo/app.py
+++ src/ldap3_demo/app.py
@@ -1,22 +1,31 @@
 #!/usr/bin/env python3
 import logging
 import sys
-
 from confuse import Configuration
 from flask import Flask, jsonify, make_response
 from flask_swagger_ui import get_swaggerui_blueprint
-from .routes import ldap_api
 from flask_cors import CORS
 
 config_root = 'ldap3_demo'
 
-app = Flask(__name__)
+
+def app_register_blueprints(app):
+    with app.app_context():
+        from .routes import ldap_api
+        ### swagger specific ###
+        SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+            app.config['swagger']['ui_url'],
+            app.config['swagger']['api_url'],
+            config={
+                'app_name': "Python-ldap-demo"
+            }
+        )
+        app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=app.config['swagger']['ui_url'])
+        ### end swagger specific ###
+        app.register_blueprint(ldap_api.get_blueprint())
 
 
-with app.app_context():
-    CORS(app)
-    config = Configuration(config_root, __name__)
-    app.config['ldap_servers'] = config['ldap_servers'].get()
+def app_logging_config(app):
     handler = logging.StreamHandler(sys.stdout)
     handler.setLevel(logging.DEBUG)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -25,18 +34,18 @@ with app.app_context():
     app.logger.setLevel(logging.DEBUG)
     app.logger.addHandler(handler)
 
-    ### swagger specific ###
-    SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
-        config['swagger']['ui_url'].get(str),
-        config['swagger']['api_url'].get(str),
-        config={
-            'app_name': "Python-ldap-demo"
-        }
-    )
-    app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=config['swagger']['ui_url'].get(str))
-    ### end swagger specific ###
 
-    app.register_blueprint(ldap_api.get_blueprint())
+def app_init(app):
+    CORS(app)
+    config = Configuration(config_root, __name__)
+    app.config['ldap_servers'] = config['ldap_servers'].get(dict)
+    app.config['swagger'] = config['swagger'].get(dict)
+    app_register_blueprints(app)
+
+
+app = Flask(__name__)
+app_init(app)
+app_logging_config(app)
 
 
 @app.errorhandler(400)
@@ -61,4 +70,3 @@ def handle_404_error(_error):
 def handle_500_error(_error):
     """Return a http 500 error to client"""
     return make_response(jsonify({'error': 'Server error'}), 500)
-
